8dbfacc1be70a076ca31354e2c207bef3e765e87
seakjh <skykjh0513@gmail.com>
2020-03-23 13:20:07 +0900

    로그인 실패시 토스트 출력
         - runOnUiThread메쏘드를 이용해서 출력
         - 안그러면 앱이 죽는다.

--- app/src/main/java/com/example/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/com/example/a20200323_01_loginandsignup/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.example.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
 import com.example.a20200323_01_loginandsignup.utils.ContextUtil;
 import com.example.a20200323_01_loginandsignup.utils.ServerUtil;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
@@ -70,7 +72,42 @@ public class MainActivity extends BaseActivity {
                     @Override
                     public void onResponse(JSONObject json) {
 
+//                        응답 실행 코드는 => 비동기 처리가 반드시 필요함.
+//                        비동기 : 다른 할 일들을 하다가, 완료되면 별도로 실행해주자.
+//                        OkHttp : 비동기 처리를 자동으로 지원 => 별도 쓰레드가 알아서 처리중
+//                        => onResponse는 다른 쓰레드가 돌리고 있다.
+//                        UI동작은 별도의 메인쓰레드가 전용으로 처리함
+//                        -> 다른쓰레드가 UI를 건드리면 앱이 터짐.
+
                         Log.d("JSON내용-메인에서", json.toString());
+
+                        try {
+                            final String message = json.getString("message");
+                            Log.d("서버가 주는 메세지", message);
+
+                            int code = json.getInt("code");
+                            Log.d("서버가 주는 코드값", code + "");
+
+                            if (code == 200) {
+//                                해당 기능이 성공적으로 동작
+                            }
+                            else{
+//                                뭔가 문제가 있었다.
+
+//                                Toast를 띄우는데 앱이 죽는다! -> UI쓰레드가 아닌데, 토스트를 띄우니까.
+//                                조치 : UI쓰레드 안에서 토스트를 띄우도록
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
                     }
                 });
