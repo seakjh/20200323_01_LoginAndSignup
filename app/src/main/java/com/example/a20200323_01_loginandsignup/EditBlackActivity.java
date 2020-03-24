0ba6f994cba54b4c6bc34af9b32b7aede4550fe2
seakjh <skykjh0513@gmail.com>
2020-03-24 17:25:28 +0900

    게시글 작성화면 기초작업

--- app/src/main/java/com/example/a20200323_01_loginandsignup/EditBlackActivity.java
+++ app/src/main/java/com/example/a20200323_01_loginandsignup/EditBlackActivity.java
@@ -1,14 +1,31 @@
 package com.example.a20200323_01_loginandsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class EditBlackActivity extends AppCompatActivity {
+import com.example.a20200323_01_loginandsignup.databinding.ActivityEditBlackBinding;
+
+public class EditBlackActivity extends BaseActivity {
+
+    ActivityEditBlackBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_black);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_black);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
