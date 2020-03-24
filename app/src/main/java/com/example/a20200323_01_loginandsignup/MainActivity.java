bde896a4ea6e6af5d7a162c35c6d252e64112108
seakjh <skykjh0513@gmail.com>
2020-03-24 15:08:27 +0900

    로그아웃 처리 구현

--- app/src/main/java/com/example/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/com/example/a20200323_01_loginandsignup/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.a20200323_01_loginandsignup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
 import com.example.a20200323_01_loginandsignup.datas.User;
+import com.example.a20200323_01_loginandsignup.utils.ContextUtil;
 import com.example.a20200323_01_loginandsignup.utils.ServerUtil;
 
 import org.json.JSONException;
@@ -28,18 +33,39 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                alert.setMessage("정말 로그아웃 하시겠습니까?");
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ContextUtil.setUserToken(mContext, "");
+                        Intent intent = new Intent(mContext, LoginActivity.class);
+                        startActivity(intent);
+
+                        finish();
+                    }
+                });
+                alert.setNegativeButton("취소", null);
+                alert.show();
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
 
-        ServerUtil.getRequestUserList(mContext, "ALL", new ServerUtil.JsonResponseHandler() {
-            @Override
-            public void onResponse(JSONObject json) {
-
-                Log.d("사용자목록", json.toString());
-            }
-        });
+//        ServerUtil.getRequestUserList(mContext, "ALL", new ServerUtil.JsonResponseHandler() {
+//            @Override
+//            public void onResponse(JSONObject json) {
+//
+//                Log.d("사용자목록", json.toString());
+//            }
+//        });
 
 //        저장된 토큰을 확인해서
 //        => 그 토큰으로 내 정보를 서버에서 다시 불러올것 - 조회 (GET).
--- app/src/main/java/com/example/a20200323_01_loginandsignup/SplashActivity.java
+++ app/src/main/java/com/example/a20200323_01_loginandsignup/SplashActivity.java
@@ -2,8 +2,11 @@ package com.example.a20200323_01_loginandsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import com.example.a20200323_01_loginandsignup.utils.ContextUtil;
 
 public class SplashActivity extends BaseActivity {
 
@@ -26,6 +29,24 @@ public class SplashActivity extends BaseActivity {
 //        토큰이 저장되어 있고 & 자동로그인이 체크되어 있다면
 //         => 메인액티비티로 이동.
         Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!ContextUtil.getUserToken(mContext).equals("") && ContextUtil.isAutoLoginCheck(mContext)) {
+
+                    Intent intent = new Intent(mContext, MainActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Intent intent = new Intent(mContext, LoginActivity.class);
+                    startActivity(intent);
+                }
+
+
+//            스플래쉬화면은 종료시키자.
+                finish();
+            }
+        }, 3000);
 
 //        그렇지 않다면 => 로그인액티비티로 이동.
 
