8ca7da38958ac26a5159265ce037795842f2e111
seakjh <skykjh0513@gmail.com>
2020-03-24 16:24:47 +0900

    블랙 게시물 리스트 아이템 그림

--- app/src/main/java/com/example/a20200323_01_loginandsignup/BoardListActivity.java
+++ app/src/main/java/com/example/a20200323_01_loginandsignup/BoardListActivity.java
@@ -7,13 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.a20200323_01_loginandsignup.databinding.ActivityBoardListBinding;
+import com.example.a20200323_01_loginandsignup.datas.Black;
 import com.example.a20200323_01_loginandsignup.utils.ServerUtil;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardListActivity extends BaseActivity {
+
+    List<Black> blacks = new ArrayList<>();
 
     ActivityBoardListBinding binding = null;
 
@@ -48,7 +54,13 @@ public class BoardListActivity extends BaseActivity {
 
                         for (int i=0; i < blackLists.length() ; i++) {
                             JSONObject bl = blackLists.getJSONObject(i);
+                            Black blackPost = Black.getBlackFromJson(bl);
+                            Log.d("블랙신고제목", blackPost.getTitle());
+
+//                            파싱끝난 블랙신고글들을 배열에 담아둠.
+                            blacks.add(blackPost);
                         }
+//                        모두 담긴 게시글들 => 어댑터가 새로고침.
 
                     }
 
